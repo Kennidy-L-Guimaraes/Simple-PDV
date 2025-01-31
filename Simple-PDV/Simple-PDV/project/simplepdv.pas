9a3341584840bec79fa398157b6ec01ac91d4927
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2025-01-31 11:43:03 -0300

    Proposed Interface Update 0.1

--- Simple-PDV/Simple-PDV/project/simplepdv.pas
+++ Simple-PDV/Simple-PDV/project/simplepdv.pas
@@ -4,11 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.jpeg;
 
 type
   TFrmSimplePDV = class(TForm)
-    Panel1: TPanel;
+    PnlBackground: TPanel;
+    ImgBackground: TImage;
+    PnlSale: TPanel;
   private
     { Private declarations }
   public
